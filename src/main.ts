1ec204e9236d16e6e72564ac6b346feadd9c635b
thislooksfun <thislooksfun@repbot.org>
2019-10-22 20:59:28 -0500

    Hardcode demo

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,41 @@
 import Promise from "bluebird";
 import server from "./web/server";
 
-import nt from "./util/webdriver";
+import queue from "./player/queue";
+
+import youtube from "./plugins/youtube";
+import sleep from "./util/sleep";
+// import spotify from "./plugins/spotify";
 
 const log = require("tlf-log");
 
 log._setLevel("debug");
 
+const devMode = process.env.NODE_ENV !== "production";
+if (devMode) {
+  require("dotenv").config();
+}
+
+const enqueueYT = (slug: string) =>
+  Promise.resolve()
+    .then(() => youtube.searchFor(slug))
+    .then(([at]) => youtube.createAudioSource(at))
+    .tap(as => queue.enqueue(as));
+
 Promise.resolve()
   // .then(tracker.init)
   .then(server.start)
-  .then(nt.bind(nt, "https://www.youtube.com/watch?v=dQw4w9WgXcQ"));
-
-// Promise
-//   .resolve()
-//   .then()
-//   // .catch()
-// ;
+  .tap(() => sleep(1000))
+  // Queue first song
+  .then(() => enqueueYT("qAeybdD5UoQ"))
+  // Queue second song
+  .then(() => enqueueYT("cHHLHGNpCSA"))
+  // Preload second song after 10s
+  .tap(() => sleep(10 * 1000))
+  .tap(() => queue.preloadNext())
+  // Start second song after 20s more
+  .tap(() => sleep(30 * 1000))
+  .then(() => queue.next())
+  // Stop after 30s
+  .tap(() => sleep(30 * 1000))
+  .then(() => queue.next());
