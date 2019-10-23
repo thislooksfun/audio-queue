be2e895596722cb7e2656dab14cad5d376e53dae
thislooksfun <thislooksfun@repbot.org>
2019-10-22 21:00:27 -0500

    Configure example http API

--- src/web/server.ts
+++ src/web/server.ts
@@ -3,12 +3,15 @@
 // Built-in
 // import path from "path";
 // import fs from "fs";
+import { execSync } from "child_process";
 // 3rd-party
 import express from "express";
+import queue from "../player/queue";
+import Promise from "bluebird";
 // Local
-// const track = require("../t");
-// const history = require("util/history");
-// const stats = require("util/stats");
+import youtube from "../plugins/youtube";
+// Logging
+import log from "tlf-log";
 
 const app = express();
 const port = parseInt(process.argv[2]) || 8080;
@@ -17,40 +20,35 @@ export default {
   start: function() {
     app.get("/", (_req, res) => res.send("Hello World!"));
 
-    // app.get("/log", (_req, res) =>
-    //   res
-    //     .set({ "Content-Type": "text/plain" })
-    //     .send(
-    //       fs.readFileSync(
-    //         path.join(__dirname, "../logs/chromedriver.log"),
-    //         "utf-8"
-    //       )
-    //     )
-    // );
+    app.get("/next", (_req, res) =>
+      queue.next().then(() => res.send("success"))
+    );
 
-    // app.get("/refresh", (_req, res) => {
-    //   log.trace("> /reload");
-    //   const start = Date.now();
-    //   return track().then(r => {
-    //     const end = Date.now();
-    //     res.send(
-    //       `Done!  -- ${JSON.stringify(r)} -- ${new Date()} -- took ${end -
-    //         start}ms`
-    //     );
-    //   });
-    // });
+    app.get("/enqueue/yt/:slug", (req, res) => {
+      Promise.resolve()
+        .then(() => youtube.searchFor(req.params.slug))
+        .then(([at]) => youtube.createAudioSource(at))
+        .tap(as => queue.enqueue(as))
+        .then(() => res.send("success"))
+        .catch((e: any) => {
+          res.status(500).send("Something went wrong!");
+          log.error(e);
+        });
+    });
+
+    app.get("/volume/:percent", (req, res) => {
+      const volume = parseFloat(req.params.percent);
+      if (isNaN(volume)) {
+        res.status(400).send("Invalid volume level");
+      } else {
+        execSync(`amixer sset PCM,0 ${volume}% > /dev/null &`);
+        res.send(`Volume set to ${volume}%`);
+      }
+    });
 
     app.get("/api/v1/ping", (_req, res) => {
       res.send("Pong!");
     });
-
-    // app.get("/api/v1/cash", (_req, res) => {
-    //   res.json(history.last);
-    // });
-
-    // app.get("/api/v1/cash-per-meal", (_req, res) => {
-    //   res.json(stats.analyze());
-    // });
 
     app.listen(port, () =>
       console.log(`Example app listening on port ${port}!`)
