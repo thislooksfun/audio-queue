4b2547aa775915aa985e3d97a71b40eaf230c2f0
thislooksfun <thislooksfun@repbot.org>
2019-10-26 15:47:48 -0500

    Add plugins/index.ts

---
+++ src/plugins/index.ts
@@ -0,0 +1,6 @@
+import { Plugin } from "./plugin";
+import youtube from "./youtube";
+import spotify from "./spotify";
+
+export const plugins: Plugin[] = [youtube, spotify];
+export default plugins;
