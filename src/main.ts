7ecbc0877a93859dd2441810ffff012ad47f5eed
thislooksfun <thislooksfun@repbot.org>
2019-10-26 13:54:58 -0500

    Clean up main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,32 +8,4 @@ import Promise from "bluebird";
 // Local
 import server from "./web/server";
 
-import queue from "./player/queue";
-
-import youtube from "./plugins/youtube";
-import sleep from "./util/sleep";
-// import spotify from "./plugins/spotify";
-
-const enqueueYT = (slug: string) =>
-  Promise.resolve()
-    .then(() => youtube.searchFor(slug))
-    .then(([at]) => youtube.createAudioSource(at))
-    .tap(as => queue.enqueue(as));
-
-Promise.resolve()
-  // .then(tracker.init)
-  .then(server.start)
-  .tap(() => sleep(1000))
-  // Queue first song
-  .then(() => enqueueYT("qAeybdD5UoQ"))
-  // Queue second song
-  .then(() => enqueueYT("cHHLHGNpCSA"))
-  // Preload second song after 10s
-  .tap(() => sleep(10 * 1000))
-  .tap(() => queue.preloadNext())
-  // Start second song after 20s more
-  .tap(() => sleep(30 * 1000))
-  .then(() => queue.next())
-  // Stop after 30s
-  .tap(() => sleep(30 * 1000))
-  .then(() => queue.next());
+Promise.resolve().then(server.start);
