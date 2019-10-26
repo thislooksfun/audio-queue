11b3a4412b84110b6a60f3846f54efd35526f988
thislooksfun <thislooksfun@repbot.org>
2019-10-26 13:53:53 -0500

    [Spotify] Define auth scopes

---
+++ src/plugins/spotify/scopes.ts
@@ -0,0 +1,18 @@
+export default [
+  // Needed to control playback
+  "user-modify-playback-state",
+
+  // Needed to see the current playing track and state
+  "user-read-currently-playing",
+  "user-read-playback-state",
+
+  // Needed for search (for some reason)
+  "user-read-private",
+
+  // Needed to read the user's library and private playlists
+  "user-library-read",
+  "playlist-read-private",
+
+  // Needed for playback via the Web API
+  "streaming",
+];
