09c56cff13eb0b8d71a5b3e508d9f7570c9605ef
thislooksfun <thislooksfun@repbot.org>
2020-01-14 22:58:39 -0600

    Configure vue-socket.io

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
+import VueSocketIO from "vue-socket.io";
 
 Vue.config.productionTip = false;
+
+Vue.use(
+  new VueSocketIO({
+    debug: true,
+    connection: io(), // eslint-disable-line no-undef
+  })
+);
 
 new Vue({
   render: h => h(App),
