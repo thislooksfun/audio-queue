594f1b8af2511c711cffb5bc4b2c8b7e09247195
thislooksfun <thislooksfun@repbot.org>
2020-01-14 22:59:37 -0600

    Add v-click-outside

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import VueSocketIO from "vue-socket.io";
+import vClickOutside from "v-click-outside";
 
 Vue.config.productionTip = false;
 
+Vue.use(vClickOutside);
 Vue.use(
   new VueSocketIO({
     debug: true,
