f6c3c9d848ec87de0aa96df097cbfa4a3c68b366
thislooksfun <thislooksfun@repbot.org>
2020-01-14 22:57:29 -0600

    Add connection status overlay

---
+++ frontend/src/components/ConnectionStatus.vue
@@ -0,0 +1,83 @@
+<template>
+  <div
+    :class="{
+      center: true,
+      'connection-status': true,
+      connected: connected,
+      hidden: hide,
+    }"
+  >
+    <span v-if="connected">Connected!</span>
+    <span v-else>Disconnected</span>
+
+    <div class="overlay"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  sockets: {
+    connect() {
+      this.connected = true;
+      this.hideto = setTimeout(() => (this.hide = true), 1 * 1000);
+    },
+    disconnect() {
+      this.connected = false;
+      this.hide = false;
+      clearTimeout(this.hideto);
+    },
+  },
+  data: () => ({ connected: false, hide: false, hideto: null }),
+};
+</script>
+
+<style lang="less" scoped>
+.connection-status {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 4rem;
+  line-height: 4rem;
+  font-size: 1.5rem;
+
+  transition: top 0.5s ease-in-out, background-color 0.25s ease-in-out;
+
+  background-color: darken(#f5365c, 10%);
+  color: #fff;
+
+  z-index: 999;
+
+  .overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    z-index: -1;
+    background-color: #0006;
+    backdrop-filter: blur(2px);
+
+    transition-property: backdrop-filter, background-color;
+    transition-duration: 0.5s;
+    transition-timing-function: ease-in-out;
+  }
+
+  &.connected {
+    background-color: darken(#2dce89, 10%);
+
+    .overlay {
+      backdrop-filter: blur(0px);
+      background-color: #0000;
+      pointer-events: none;
+    }
+  }
+  &.hidden {
+    top: -5rem;
+
+    .overlay {
+      display: none;
+    }
+  }
+}
+</style>
